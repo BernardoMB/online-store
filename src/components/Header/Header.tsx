d39a257a67853fbc4ef082a1a2b3b40c4ecd142b
BernardoMB <bmondragonbrozon@gmail.com>
2025-07-20 02:42:44 -0400

    Improved applicaiton header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
-import { Badge, Box, Icon, Stack, Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { useCartTotals } from "../../hooks/useCart";
-import { ColorModeButton } from "../ui/color-mode";
+import { ColorModeButton, useColorModeValue } from "../ui/color-mode";
 import './Header.css';
-import { FaShoppingCart } from "react-icons/fa";
 import { CartIndicator } from "../CartIndicator";
+import { StoreLogo } from "../StoreLogo";
 
 type HeaderProps = {
   expanded: boolean;
@@ -20,21 +20,33 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   const { price, count } = useCartTotals();
 
+  const logoColor = useColorModeValue('black', 'white');
+
   return (
     <header className="site-header">
-      <button onClick={toggleSidebar}>
-        {isSidebarOpen ? <div>&#10006;</div> : "☰"}
-      </button>
-      <Box whiteSpace="nowrap">
-        &emsp;🦉 Hue & Hoot
-      </Box>
-      {/* <button onClick={toggleHeader} style={{ marginLeft: "1rem" }}>
-        {expanded ? "Collapse Header" : "Expand Header"}
-      </button> */}
-      <div className="spacer"></div>
-      <ColorModeButton></ColorModeButton>
-      &emsp;
-      <CartIndicator count={count} price={price}></CartIndicator>
+      <div className="header-container">
+        <div className="header-section">
+          <button onClick={toggleSidebar}>
+            {isSidebarOpen ? <div>&#10006;</div> : "☰"}
+          </button>
+          &emsp;
+        </div>
+        <div className="header-section">
+          <StoreLogo color={logoColor} width="35" height="46" />
+          &ensp;
+          <Box whiteSpace="nowrap">
+            <Text textStyle="xl">Hue & Hoot</Text>
+          </Box>
+        </div>
+        {/* <button onClick={toggleHeader} style={{ marginLeft: "1rem" }}>
+          {expanded ? "Collapse Header" : "Expand Header"}
+        </button> */}
+        {/* <div className="spacer"></div> */}
+        <div className="header-section">
+          <ColorModeButton></ColorModeButton>
+          <CartIndicator count={count} price={price}></CartIndicator>
+        </div>
+      </div>
     </header>
   );
 };
